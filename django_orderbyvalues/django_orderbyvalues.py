157c7fb1e2941353302df79026f88ac2699d0b03
翟放 <1069263908@qq.com>
2019-06-19 16:18:51 +0800

    sql语句使用大写

--- django_orderbyvalues/django_orderbyvalues.py
+++ django_orderbyvalues/django_orderbyvalues.py
@@ -8,7 +8,7 @@ def order_by_values(self, field, values, temp_name='ordering_values'):
     if field == 'pk':
         field = self.model._meta.db_table + '.' + self.model._meta.pk.name
     value_list = ', '.join(['"%s"' % value if isinstance(value, str) else value for value in values])
-    sql = 'field(%s, %s)' % (field, value_list)
+    sql = 'FIELD(%s, %s)' % (field, value_list)
     return self.extra(select={temp_name: sql}, order_by=(temp_name,))
 
 
